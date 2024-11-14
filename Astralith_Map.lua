6e106f4bdd579f1a5ea966ad1864752cd0d539e4
Devetik <sebastien@cuttat.dev>
2024-11-14 16:51:34 +0100

    p5

--- Astralith_Map.lua
+++ Astralith_Map.lua
@@ -18,7 +18,7 @@ local function SendPlayerPosition()
 
     if x and y then
         local message = string.format("%s|%f|%f|%d|%d|%s", name, x, y, mapID, level, class)
-        C_ChatInfo.SendAddonMessage("Astralith_Map", message, "PARTY")
+        C_ChatInfo.SendAddonMessage("Astralith_Map", message, "GUILD")
         print(string.format("Envoi position : %s - X: %f, Y: %f, MapID: %d", name, x, y, mapID))
     end
 end
